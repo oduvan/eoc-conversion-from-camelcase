a87ca1aa93e9b739a29e7ebacb758fcdc6d1755d
Alexander Lyabah <a.lyabah@checkio.org>
2019-04-09 09:59:09 +0300

    first commit

---
+++ verification/src/tests.py
@@ -0,0 +1,36 @@
+"""
+TESTS is a dict with all of your tests.
+Keys for this will be the categories' names.
+Each test is a dict with
+    "input" -- input data for a user function
+    "answer" -- your right answer
+    "explanation" -- not necessarily a key, it's used for an additional info in animation.
+"""
+
+
+TESTS = {
+    "Basics": [
+        {
+            "input": "MyFunctionName",
+            "answer": "my_function_name"
+        },
+        {
+            "input": "IPhone",
+            "answer": "i_phone"
+        }
+    ],
+    "Extra": [
+        {
+            "input": "ThisFunctionIsEmpty",
+            "answer": "this_function_is_empty"
+        },
+        {
+            "input": "Name",
+            "answer": "name"
+        },
+	{
+            "input": "ThisIsReallyVeryLongString",
+            "answer": "this_is_really_very_long_string"
+        }
+    ]
+}
